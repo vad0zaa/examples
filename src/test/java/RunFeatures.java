5418ae0271dea28e0ec9ea5a884c89fe93e88d43
unknown <Administrator@EE7-VSINCHUK-L.zone2.flir.net>
2016-07-30 14:27:34 +0300

    Initial commit

---
+++ src/test/java/RunFeatures.java
@@ -0,0 +1,10 @@
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+/**
+ * Created by Administrator on 28.07.2016.
+ */
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src/test/java/features")
+public class  RunFeatures{
+}
