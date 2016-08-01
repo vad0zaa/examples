74fefba72b9613fd58755ee70fb894cf57c173b7
Administrator <vad0zaa@mail.ru>
2016-08-01 13:05:04 +0300

    added - cucumber options ,pretty, html target, json target

--- src/test/java/RunFeatures.java
+++ src/test/java/RunFeatures.java
@@ -5,6 +5,6 @@ import org.junit.runner.RunWith;
  * Created by Administrator on 28.07.2016.
  */
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/java/features")
+@CucumberOptions(features="src/test/java/features", format = {"pretty", "html:target/cucumber", "json:target/cucumber.json"})
 public class  RunFeatures{
 }
